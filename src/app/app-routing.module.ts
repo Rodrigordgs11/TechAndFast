be2ac0ea3de5fcfe5784cf7d2b200263c4ca4385
Rodrigo <rodribola.0886@gmail.com>
2023-05-07 17:06:09 +0100

    add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,7 +18,23 @@ const routes: Routes = [
   {
     path: 'layout',
     loadChildren: () => import('./layout/layout.module').then( m => m.LayoutPageModule)
+  },  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   },
+  {
+    path: 'reset-password',
+    loadChildren: () => import('./reset-password/reset-password.module').then( m => m.ResetPasswordPageModule)
+  },
+  {
+    path: 'client-area',
+    loadChildren: () => import('./client-area/client-area.module').then( m => m.ClientAreaPageModule)
+  },
+  {
+    path: 'menu',
+    loadChildren: () => import('./menu/menu.module').then( m => m.MenuPageModule)
+  },
+
 ];
 
 @NgModule({

9c8fe1ce8a4848451154619c6862ad32805c95f4
Rodrigo <rodribola.0886@gmail.com>
2023-05-12 15:31:57 +0100

    implemet dinamic page with activeted routes

--- src/app/product-detail/product-detail.page.ts
+++ src/app/product-detail/product-detail.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpConnectionService } from '../services/http-connection.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Product } from '../models/product';
 
 @Component({
   selector: 'app-product-detail',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-detail.page.scss'],
 })
 export class ProductDetailPage implements OnInit {
+  product: Product = {} as Product;
+  productId: any;
 
-  constructor() { }
-
+  constructor(private http: HttpConnectionService, public router: Router, private route: ActivatedRoute) {
+    this.productId = this.route.snapshot.paramMap.get('productId');
+  }
+  
   ngOnInit() {
+    this.productAtributes(this.productId);
   }
 
+  productAtributes(productid: number) {
+    this.http.get<any>('products/' + productid).subscribe(res => {
+      if(res != null) { 
+        console.log(res) 
+        this.product = res;
+      }
+    },
+      (error: any) => {
+        if(error.status === 401) {
+          this.router.navigate(['/login']);
+        }
+    })
+  }
 }

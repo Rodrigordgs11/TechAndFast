3f553bc848279a1168709c19bd04f3476156b895
Rodrigo <rodribola.0886@gmail.com>
2023-05-12 15:30:53 +0100

    implement refresh access token

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,22 +1,32 @@
 import { HttpClient, HttpHeaders, HttpResponse, HttpErrorResponse } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
 import { User } from '../models/user';
 import { Observable, map, interval } from 'rxjs';
 import { Router } from '@angular/router';
-import { error } from 'console';
+import { AlertController } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root'
 })
 
-export class AuthServiceService {
+export class AuthServiceService{
 
   endPoint: string = 'http://185.113.143.51:8081/api/v1/auth'
   headers = new HttpHeaders().set('Content-Type', 'application/json')
   currentUser = {}
-  msg: any = '';
 
-  constructor(private http: HttpClient, public router: Router) {}
+  constructor(private http: HttpClient, public router: Router, public alertController: AlertController) {}
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Login Error',
+      subHeader: 'Username or password are invalids',
+      message: 'Please introduce correct credentials',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
 
   signIn(username: any, password: any) {
     const userObj = { username, password };
@@ -33,8 +43,7 @@ export class AuthServiceService {
         //});
     }, (error: any) => {
       if(error.status === 401) {
-        console.log('Error:', error.status);
-        this.msg = error.status;
+        this.presentAlert();
       }
     });
   }
@@ -50,14 +59,15 @@ export class AuthServiceService {
           'Authorization': 'Bearer ' + localStorage.getItem('access_token')
         })
       };
-      interval(50000).subscribe(() => {
-        this.http.post<any>(this.endPoint + '/refresh-token', null, httpOptions)
-          .subscribe((res: any) => {
-            console.log(res.access_token);
-          }, (error: any) => {
-            console.error(error);
-          });
-      });
+      
+      this.http.post<any>(this.endPoint + '/refresh-token', null, httpOptions)
+        .subscribe((res: any) => {
+          localStorage.setItem('access_token', res.access_token)
+          console.log(localStorage.getItem('access_token'));
+        }, (error: any) => {
+          console.error(error);
+        });
     }
   }
+  
 }

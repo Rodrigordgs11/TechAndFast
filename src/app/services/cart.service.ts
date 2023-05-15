89fa543cbb6c54b39a8fadbf379d9e98eff42ca6
Rodrigo <rodribola.0886@gmail.com>
2023-05-15 20:46:10 +0100

    create and implement feautures cart service

---
+++ src/app/services/cart.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import { Product } from '../models/product';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+
+  cartItems: { product: Product, quantity: number }[] = [];
+  productCountSubject: BehaviorSubject<number> = new BehaviorSubject<number>(0);
+  totalPriceSubject: BehaviorSubject<number> = new BehaviorSubject<number>(0);
+
+  removeFromCart(index: number): void {
+    this.cartItems.splice(index, 1);
+    this.updateProductCount();
+    this.updateTotalPrice();
+  }
+
+  getCartItems(): { product: Product, quantity: number }[] {
+    return this.cartItems;
+  }
+
+  addToCart(product: Product, quantity: number): void {
+    const index = this.cartItems.findIndex(item => item.product.id === product.id);
+    if (index !== -1) {
+      this.cartItems[index].quantity += quantity;
+    } else {
+      this.cartItems.push({ product, quantity });
+    }
+    this.updateProductCount();
+    this.updateTotalPrice();
+  }
+
+  updateProductCount(): void {
+    this.productCountSubject.next(this.cartItems.length);
+  }
+
+  getProductCount(): BehaviorSubject<number> {
+    return this.productCountSubject;
+  }
+
+  updateTotalPrice(): void {
+    const totalPrice = this.cartItems.reduce((total, item) => {
+      return total + (item.product.price * item.quantity);
+    }, 0);
+    this.totalPriceSubject.next(totalPrice);
+  }
+
+  getTotalPrice(): BehaviorSubject<number> {
+    return this.totalPriceSubject;
+  }
+}

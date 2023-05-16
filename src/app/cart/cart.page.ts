aab1220c0d984e4b63b0b088d1082dce31367cc5
Rodrigo <rodribola.0886@gmail.com>
2023-05-16 19:05:21 +0100

    update cart

--- src/app/cart/cart.page.ts
+++ src/app/cart/cart.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from '../services/cart.service';
+import { Product } from '../models/product';
 
 @Component({
   selector: 'app-cart',
@@ -7,9 +9,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartPage implements OnInit {
 
-  constructor() { }
+  productCount: number = 0;
+  totalPrice: number = 0;
+  cartItems: { product: Product, quantity: number }[] = [];
+
+  constructor(private cartService:CartService) { }
 
   ngOnInit() {
+
+    this.cartItems = this.cartService.getCartItems();
+
+    this.cartService.getProductCount().subscribe(count => {
+      this.productCount = count;
+    });
+
+    this.cartService.getTotalPrice().subscribe(price => {
+      this.totalPrice = price;
+    });
   }
 
+  removeProduct(id: number) {
+    this.cartService.removeFromCart(id);
+  }
+
+  incrementQuantity(id: number){
+    const index = this.cartItems.findIndex(item => item.product.id === id);
+    if(this.cartItems[index].quantity > 0){
+      this.cartItems[index].quantity++;
+    }
+  }
+
+  descrementQuantity(id: number){
+    const index = this.cartItems.findIndex(item => item.product.id === id);
+    if(this.cartItems[index].quantity > 1){
+      this.cartItems[index].quantity--;
+    }
+  }
 }

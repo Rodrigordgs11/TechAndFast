0d25c6a62a1af12bf07c5a2bf8b0587f71b1912f
Rodrigo <rodribola.0886@gmail.com>
2023-05-24 02:43:39 +0100

    minor changes

---
+++ src/app/components/header/header.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { HeaderComponent } from './header.component';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  declarations: [
+    HeaderComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule
+  ],
+  exports: [
+    HeaderComponent
+  ]
+})
+export class HeaderModule { }

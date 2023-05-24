50b696ba0ff7a5c2f36de002be9b0ae86d05231e
Rodrigo <rodribola.0886@gmail.com>
2023-05-24 02:45:26 +0100

    create pack model

---
+++ src/app/models/pack.ts
@@ -0,0 +1,11 @@
+export interface Pack {
+    id: any;
+    name: string;
+    image: any;
+    price: any;
+    stock: any;
+    isActive: boolean;
+    category: any;
+    tax: any;
+    isPack: boolean;
+}

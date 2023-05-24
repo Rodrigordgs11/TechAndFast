612b152203abb4dbcce59dc7d3aa7b3d17517b2e
Rodrigo <rodribola.0886@gmail.com>
2023-05-24 14:58:14 +0100

    create user model

---
+++ src/app/models/user.ts
@@ -0,0 +1,12 @@
+export interface User  {
+    id: any;
+    address: string;
+    city: string;
+    email: any;
+    fiscalId: number;
+    isActive: boolean;
+    isDeleted: boolean;
+    name: string;
+    phone: number;
+    zipCode 
+}
